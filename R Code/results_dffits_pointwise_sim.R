f2ecc20c87872bf891a7e3b32dc7606df20947b9
Michael Creutzinger <creutzml@Michaels-iMac.local>
2024-02-02 15:05:27 -0700

    - adding simulation results and script

--- R Code/results_dffits_pointwise_sim.R
+++ R Code/results_dffits_pointwise_sim.R
@@ -20,22 +20,18 @@ library(fda)
 library(ffscb)
 library(conformalInference.fd)
 library(progress)
+library(magrittr)
 
-# Main directory:
-main_dir <- file.path("/Users/creutzml/Library/Mobile Documents",
-                      "/com~apple~CloudDocs/Documents/Dissertation",
-                      "/functional_data_analysis/code")
-source(file.path(main_dir, 
-                 "fast_and_fair/make_regression_band_mc.R"))
-source(file.path(main_dir, 
-                 "fast_and_fair/make_band_mc.R"))
-source(file.path(main_dir, 
-                 "fast_and_fair/confidence_band_mc.R"))
-source(file.path(main_dir, 
-                 "fast_and_fair",
-                 "make_concurrent_regression_band_mc_10_12_23.R"))
-source(file.path(main_dir, 
-                 "functional_dffits/fRegress_concurrent.R"))
+# Install ffscbExtra
+devtools::install_github("creutzml/ffscbExtra")
+library(ffscbExtra)
+
+# Directories
+functions_path <- file.path(here::here(), "R Code")
+data_path <- file.path(here::here(), "Data")
+
+
+# Source Pittman's functions
 source(file.path(main_dir, 
                  "functional_dffits/pittman_data_gen.R"))
 #####################################################################
@@ -44,92 +40,147 @@ source(file.path(main_dir,
 
 ### Investigate the results
 #####################################################################
-load(paste0("/Users/creutzml/Library/Mobile Documents",
-            "/com~apple~CloudDocs/Documents/Dissertation",
-            "/functional_data_analysis/sim_results",
-            "/functional_dffits/dffits_exp_mc_11_2_23.RData"))
+load(file.path(data_path, "dffits_exp_mod1_df_11_23_23.RData"))
 
-# Pivot longer:
-sim_results_df_longer <- sim_results_df %>%
-  pivot_longer(cols = alpha_0.1000:alpha_0.0015, 
-               names_to = "alpha", 
-               values_to = "n_out")
+# sim_results_df2 %<>%
+#   dplyr::mutate(
+#     mcc = (n_tp*n_tn - n_fp*n_fn)/
+#       sqrt((n_tp + n_fp)*(n_tp + n_fn)*(n_tn + n_fp)*(n_tn + n_fn)))
+# 
+sim_results_df_sum <- sim_results_df %>%
+  # dplyr::bind_rows(sim_results_df2) %>%
+  dplyr::mutate(precision = n_tp/(n_tp + n_fp)) %>%
+  dplyr::mutate(fpr = 1 - specificity) %>%
+  # dplyr::select(-c(n_tp, n_fp, n_tn, n_fn)) %>%
+  dplyr::group_by(n, lambda, n_inf, alpha_val, sp) %>%
+  dplyr::summarise(avg_n_tp = mean(n_tp), 
+                   avg_n_fp = mean(n_fp), 
+                   avg_n_tn = mean(n_tn), 
+                   avg_n_fn = mean(n_fn), 
+                   avg_fpr = mean(fpr),
+                   avg_sensitivity = mean(sensitivity),
+                   sd_sensitivity = sd(sensitivity, na.rm = T),
+                   avg_specificity = mean(specificity),
+                   sd_specificity = sd(specificity, na.rm = T),
+                   avg_accuracy = mean(accuracy),
+                   sd_accuracy = sd(accuracy, na.rm = T),
+                   avg_mcc = mean(mcc, na.rm = T),
+                   sd_mcc = sd(mcc, na.rm = T), 
+                   avg_precision = mean(precision, na.rm = T), 
+                   sd_precision = sd(precision, na.rm = T))
 
-# Take a look at some boxplots:
+
+
+# # Pivot longer:
+# sim_results_df_sum <- sim_results_df %>%
+#   dplyr::bind_rows(sim_results_df2) %>%
+#   dplyr::select(-c(n_tp, n_fp, n_tn, n_fn)) %>%
+#   dplyr::group_by(n, lambda, n_inf, alpha_val, sp) %>%
+#   dplyr::summarise(avg_sensitivity = mean(sensitivity), 
+#                    sd_sensitivity = sd(sensitivity, na.rm = T),
+#                    avg_specificity = mean(specificity),  
+#                    sd_specificity = sd(specificity, na.rm = T),
+#                    avg_accuracy = mean(accuracy),
+#                    sd_accuracy = sd(accuracy, na.rm = T),
+#                    avg_mcc = mean(mcc, na.rm = T),
+#                    sd_mcc = sd(mcc, na.rm = T))
+
+# Colorblind palette for plotting
+cb_pallette <- c("#E69F00", "#0072B2", "black", "#006b4e")
+
+# Take a look at some plots:
+sim_results_df_sum %>%
+  mutate(n = factor(n), 
+         n_inf = factor(n_inf)) %>% 
+         # lambda = lambda + 1,
+         #alpha_val = factor(alpha_val)) %>%
+  filter(sp != 0, alpha_val == 0.1) %>%
+  rename("# Influential" = "n_inf", 
+         "lam" = "lambda") %>%
 ggplot() +
-  geom_boxplot(aes(fill = influential, y = n_out, x = alpha),
-               data = sim_results_df_longer) +
-  geom_hline(yintercept = 100, linetype = "dashed") +
-  facet_grid(rows = vars(n), cols = vars(lambda)) +
-  theme(axis.text.x = element_text(angle = 45))
+  # geom_point(aes(x = sp, y = avg_accuracy, 
+  #                color = alpha_val)) +
+  geom_line(aes(x = sp, y = avg_mcc, color = n)) +
+  # geom_errorbar(aes(x = sp, 
+  #                   ymin = avg_accuracy - 2*sd_accuracy/sqrt(n), 
+  #                   ymax = avg_accuracy + 2*sd_accuracy/sqrt(n),
+  #                   color = alpha_val)) +
+  facet_grid(cols = vars(`# Influential`), rows = vars(lam), 
+             labeller = label_bquote(
+               rows = lambda*" = "*.(lam), 
+               cols = "# Influential = "*.(`# Influential`))) +
+  # facet_grid(cols = vars(n), rows = vars(lam), 
+  #            labeller = label_bquote(
+  #              rows = lambda*" = "*.(lam), 
+  #              cols = "n = "*.(n))) +
+  scale_color_manual(values = cb_pallette) +
+  scale_x_continuous(expand = c(0,0)) +
+  labs(x = "Sampling Point (t)", 
+       y = "Avg. Matthew's Correlation Coefficient (MCC)", 
+       color = expression(alpha*" Quantile")) +
+  theme_bw() +
+  theme(text = element_text(size = 16), 
+        plot.margin = unit(c(.5, 1, 1, 1), "cm"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
-## 99.7%ile cutoff:
-sim_results_df_longer_997 <- sim_results_df_longer %>% 
-  filter(alpha == "alpha_0.0015")
-ggplot() +
-  geom_boxplot(aes(x = influential, y = n_out),
-               data = sim_results_df_longer_997) +
-  # geom_hline(yintercept = 100, linetype = "dashed") +
-  facet_grid(rows = vars(n), cols = vars(lambda))
 
-# Summary
-sim_results_df_sum_997 <- sim_results_df_longer_997 %>%
-  mutate(n_out_over_30 = n_out > 30, 
-         n_out_over_100 = n_out > 100) %>%
-  pivot_longer(c(n_out_over_30, n_out_over_100), 
-               names_to = "rule", values_to = "inf_est") %>%
-  group_by(n, lambda, rule) %>%
-  mutate(tp = case_when(
-    influential & inf_est ~ 1, 
-    TRUE ~ 0
-  ), 
-  tn = case_when(
-    !influential & !inf_est ~ 1, 
-    TRUE ~ 0
-  ), 
-  fp = case_when(
-    !influential & inf_est ~ 1, 
-    TRUE ~ 0
-  ), 
-  fn = case_when(
-    influential & !inf_est ~ 1, 
-    TRUE ~ 0
-  )) %>%
-  summarize(N = n(), 
-            ntp = sum(tp), 
-            ntn = sum(tn), 
-            nfp = sum(fp), 
-            nfn = sum(fn))
-# log_model <- glm(influential ~ n + lambda + n_out, 
-#                  data = sim_results_df_longer_997, 
-#                  family = binomial)
-# summary(log_model)
-# sim_results_df_longer_997$log_preds <- log_model$fitted.values
-# sim_results_df_longer_997 %<>% 
-#   dplyr::arrange(n_out)
-# plot(log_preds ~ n_out, 
-#      data = sim_results_df_longer_997,
-#      type = "l",
-#      lwd = 2)
-
-# Summary stats
-sim_results_summary <- sim_results_df_longer %>%
-  group_by(n, lambda, influential, alpha) %>%
-  summarize(n_2.5 = quantile(n_out, probs = 0.025),
-            n_mean = mean(n_out, na.rm = T),
-            n_median = median(n_out, na.rm = T),
-            n_97.5 = quantile(n_out, probs = 0.975))
-
-# Plot of summary stats
-ggplot() +
-  geom_line(aes(x = n, y = n_median, color = influential), 
-            data = sim_results_summary) + 
-  geom_point(aes(x = n, y = n_median, color = influential), 
-             data = sim_results_summary) +
-  facet_grid(rows = vars(alpha), cols = vars(lambda))
-
-# Differenes in counts boxplot... can I?
-sim_results_diff <- sim_results_df_longer %>%
-  pivot_wider(names_from = influential)
 #####################################################################
 
+sim_results_df_sum_r <- sim_results_df %>%
+  # dplyr::bind_rows(sim_results_df2) %>%
+  dplyr::mutate(precision = n_tp/(n_tp + n_fp)) %>%
+  dplyr::mutate(fpr = 1 - specificity,
+                n_inf_p = n_inf/n) %>%
+  # dplyr::select(-c(n_tp, n_fp, n_tn, n_fn)) %>%
+  dplyr::group_by(n_inf_p, lambda, alpha_val, sp) %>%
+  dplyr::summarise(avg_n_tp = mean(n_tp), 
+                   avg_n_fp = mean(n_fp), 
+                   avg_n_tn = mean(n_tn), 
+                   avg_n_fn = mean(n_fn), 
+                   avg_fpr = mean(fpr),
+                   avg_sensitivity = mean(sensitivity),
+                   sd_sensitivity = sd(sensitivity, na.rm = T),
+                   avg_specificity = mean(specificity),
+                   sd_specificity = sd(specificity, na.rm = T),
+                   avg_accuracy = mean(accuracy),
+                   sd_accuracy = sd(accuracy, na.rm = T),
+                   avg_mcc = mean(mcc, na.rm = T),
+                   sd_mcc = sd(mcc, na.rm = T), 
+                   avg_precision = mean(precision, na.rm = T), 
+                   sd_precision = sd(precision, na.rm = T))
+
+# Take a look at some plots:
+sim_results_df_sum_r %>%
+  # mutate(n_inf_p = factor(n_inf_p)) %>% 
+  # lambda = lambda + 1,
+  mutate(alpha_val = factor(alpha_val)) %>%
+  filter(sp != 0) %>%
+  rename(#"Proportion Influential" = "n_inf", 
+         "lam" = "lambda") %>%
+  ggplot() +
+  # geom_point(aes(x = sp, y = avg_accuracy, 
+  #                color = alpha_val)) +
+  geom_line(aes(x = sp, y = avg_mcc, color = alpha_val)) +
+  # geom_errorbar(aes(x = sp, 
+  #                   ymin = avg_accuracy - 2*sd_accuracy/sqrt(n), 
+  #                   ymax = avg_accuracy + 2*sd_accuracy/sqrt(n),
+  #                   color = alpha_val)) +
+  facet_grid(cols = vars(n_inf_p), rows = vars(lam), 
+             labeller = label_bquote(
+               rows = lambda*" = "*.(lam), 
+               cols = "Prop. Inf. = "*.(n_inf_p))) +
+  # facet_grid(cols = vars(n), rows = vars(lam), 
+  #            labeller = label_bquote(
+  #              rows = lambda*" = "*.(lam), 
+  #              cols = "n = "*.(n))) +
+  scale_color_manual(values = cb_pallette) +
+  scale_x_continuous(expand = c(0,0)) +
+  labs(x = "Sampling Point (t)", 
+       y = "Avg. Matthew's Correlation Coefficient (MCC) ", 
+       color = expression(alpha*" Quantile")) +
+  theme_bw() +
+  theme(text = element_text(size = 16), 
+        plot.margin = unit(c(.5, 1, 1, 1), "cm"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
